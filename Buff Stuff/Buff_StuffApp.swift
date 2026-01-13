1bd356de4d955770f4e86a0f8809827427b7c507
umairahmed <umair.ahm00@gmail.com>
2026-01-13 18:50:26 -0500

    Initial Commit

---
+++ Buff Stuff/Buff_StuffApp.swift
@@ -0,0 +1,17 @@
+//
+//  Buff_StuffApp.swift
+//  Buff Stuff
+//
+//  Created by Umair Ahmed on 1/13/26.
+//
+
+import SwiftUI
+
+@main
+struct Buff_StuffApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
